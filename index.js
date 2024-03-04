c5a501b663b3fc013e4d3f71cf97cce9d19a84fe
santhosh2767 <sk@santhosh.gmail.com>
2024-03-04 22:22:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const currencyFirstEl = document.getElementById('currency-first')
+const worthFirstEl = document.getElementById('worth-first')
+const currencySecondEl = document.getElementById('currency-second')
+const worthSecondEl = document.getElementById('worth-second')
+const exchangeRateEl = document.getElementById('exchange-rate')
+
+updateRate()
+
+function updateRate(){
+        fetch(`https://v6.exchangerate-api.com/v6/02e330a1e39c3d0e04334a7d/latest/${currencyFirstEl.value}`).then((res) => res.json())
+        .then((data)=>{
+                const rate = data.conversion_rates[currencySecondEl.value]
+                exchangeRateEl.innerText = `1 ${currencyFirstEl.value} = ${rate + " " + currencySecondEl.value}`
+                worthSecondEl.value = (worthFirstEl.value*rate).toFixed(2)
+}); }
+
+currencyFirstEl.addEventListener('change', updateRate)
+
+currencySecondEl.addEventListener('change', updateRate)
+
+worthFirstEl.addEventListener('change', updateRate)
